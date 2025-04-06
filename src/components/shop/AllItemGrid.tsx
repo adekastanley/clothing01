35462299d3014988882d9b67857c29a20e0c15f0
adeka stanley <adekastanley2@gmail.com>
2025-04-06 21:36:23 +0100

    whole lotta changes fix 1

--- src/components/shop/AllItemGrid.tsx
+++ src/components/shop/AllItemGrid.tsx
@@ -1,5 +1,5 @@
 "use client";
-import Link from "next/link";
+// import Link from "next/link";
 
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import ShoppingCard from "./shopping-card";
